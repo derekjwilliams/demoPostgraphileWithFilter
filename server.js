c8891929129c87fdcfc0c4e7890d01547d69e7ed
Derek Williams <derekwilliams@Dereks-MacBook-Pro.local>
2019-07-16 07:45:36 -0600

    updated postgraphile package, added posgis and graphile/pg-pubsub plugins

--- server.js
+++ server.js
@@ -4,15 +4,6 @@ const { ApolloServer } = require("apollo-server");
 const { makeSchemaAndPlugin } = require("postgraphile-apollo-server");
 const ConnectionFilterPlugin = require("postgraphile-plugin-connection-filter");
 
-const postGraphileOptions = {
-  //jwtSecret: process.env.JWT_SECRET || String(Math.random())
-  appendPlugins: [ConnectionFilterPlugin],
-  graphql: true,
-  graphiql: true,
-  dynamicJson: true,
-  classicIds: true,
-};
-
 const dbSchema = process.env.SCHEMA_NAMES
   ? process.env.SCHEMA_NAMES.split(",")
   : "public";
@@ -21,13 +12,23 @@ const pgPool = new pg.Pool({
   connectionString: (process.env.DATABASE_URL || 'postgres://localcoreapi:localcoreapi@127.0.0.1:5432/localcoreapi'),
 });
 
+const postGraphileOptions = {
+  //jwtSecret: process.env.JWT_SECRET || String(Math.random())
+  appendPlugins: [ConnectionFilterPlugin],
+  graphql: true,
+  graphiql: true,
+  dynamicJson: true,
+  classicIds: true,
+  legacyRelations: 'omit',
+};
+
 console.log(JSON.stringify(pgPool))
 
 async function main() {
   // See https://www.graphile.org/postgraphile/usage-schema/ for schema-only usage guidance
   const { schema, plugin } = await makeSchemaAndPlugin(
     pgPool,
-    'public',
+    dbSchema,
     postGraphileOptions
   );
 
